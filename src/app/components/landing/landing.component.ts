9984bec7c65f3a6bcc10633f875eae677bea9620
Pradumna Garg <pradumnagarg@gmail.com>
2024-10-23 00:31:33 +0530

    clients

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,28 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { TeamService } from 'src/app/services/team.service';
-import emailjs from '@emailjs/browser';
-import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { TeamService } from "src/app/services/team.service";
+import emailjs from "@emailjs/browser";
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.scss"],
 })
 export class LandingComponent implements OnInit {
-
-  constructor (private teamService: TeamService,
-               private fb: FormBuilder) {}
-  team: any[] = []
+  constructor(private teamService: TeamService, private fb: FormBuilder) {}
+  team: any[] = [];
   contactForm!: FormGroup;
-  buttonText = 'Send';
+  buttonText = "Send";
   buttonEnable = true;
 
   templateParams = {
-    name: 'James',
-    message: 'Check this out!',
-    email: 'email@gmail.com',
-    contact: '12421412'
-};
+    name: "James",
+    message: "Check this out!",
+    email: "email@gmail.com",
+    contact: "12421412",
+  };
 
   ngOnInit(): void {
     this.team = this.teamService.team;
@@ -44,31 +47,34 @@ export class LandingComponent implements OnInit {
     }
 
     const payload = {
-      ...this.contactForm.value
-    }
-    
-    this.buttonText = 'Sending';
+      ...this.contactForm.value,
+    };
+
+    this.buttonText = "Sending";
     this.buttonEnable = false;
-    emailjs.send('service_ggdq7vq','template_2mfqk86', payload, 'lDhSkUNlQpbm39lr4')
-	  .then((response) => {
-      this.buttonText = "Message sent!";
-      this.contactForm.reset();
+    emailjs
+      .send("service_ggdq7vq", "template_2mfqk86", payload, "lDhSkUNlQpbm39lr4")
+      .then(
+        (response) => {
+          this.buttonText = "Message sent!";
+          this.contactForm.reset();
 
-      setTimeout(() => {
-        this.buttonEnable = true;
-        this.buttonText = "Send";
-      }, 2000);
-	    
-      console.log('SUCCESS!', response.status, response.text);
-    }, (err) => {
-      
-      this.buttonText = "Failed, Try again later";
+          setTimeout(() => {
+            this.buttonEnable = true;
+            this.buttonText = "Send";
+          }, 2000);
 
-      setTimeout(() => {
-        this.buttonEnable = true;
-        this.buttonText = "Send";
-      }, 2000);
-      console.log('FAILED...', err);
-    });
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        (err) => {
+          this.buttonText = "Failed, Try again later";
+
+          setTimeout(() => {
+            this.buttonEnable = true;
+            this.buttonText = "Send";
+          }, 2000);
+          console.log("FAILED...", err);
+        }
+      );
   }
 }
